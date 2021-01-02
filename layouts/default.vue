a6c51a3b4f4f532426bbc50c51553978d13a6272
Casey Kulm <mlukyesac@gmail.com>
2021-01-01 22:06:33 -0700

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <nav
+      class="navbar header has-shadow is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a
+          class="navbar-item"
+          href="/"
+        >
+          <img
+            src="~assets/bee.png"
+            alt="Bee Icon"
+            height="28"
+          >
+        </a> 
+      </div>
+    </nav>
+
+    <section class="main-content columns">
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section>
+
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <div>
+          <p> Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> </p>
+          <p> Solver made by <a href="http://caseykulm.com">Casey Kulm</a></p>
+        </div>
+      </div>      
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <div class="column"></div>
+      <div class="column is-narrow">
+        <b-field label="Spelling Bee Letters">
+          <b-input 
+            placeholder="7 unique letters" 
+            rounded 
+            maxlength="7" 
+            type="text" 
+            validation-message="Must be 7 unique lower case letters" 
+            pattern="(?:([a-z])(?!.*\1))*" >
+          </b-input>
+        </b-field>
+
+        <b-field label="Center Letter">
+          <b-input 
+            placeholder="center letter" 
+            rounded 
+            maxlength="1" 
+            type="text" 
+            validation-message="Must be one of the 7 unique letters" 
+            pattern="[a-z]">
+          </b-input>
+        </b-field>
+        <div class="columns is-mobile">
+          <div class="column"></div>
+          <div class="column">
+            <b-button type="is-primary">Solve</b-button>
+          </div>
+          <div class="column"></div>
+        </div>
+      </div>
+      <div class="column"></div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
